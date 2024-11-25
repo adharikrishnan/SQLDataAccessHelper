e7bad944f4935e3bd7ff5ebc55b5f16d02ce237a
= <=>
2024-11-25 23:32:35 +0530

    added and updated intial framework

--- SQLDataAccess/Common/Exceptions/DataAccessException.cs
+++ SQLDataAccess/Common/Exceptions/DataAccessException.cs
@@ -10,13 +10,46 @@ namespace SQLDataAccess.Common.Exceptions
         /// <summary>
         /// The Command Text.
         /// </summary>
-        public string CommandText { get; }
+        public string? CommandText { get; }
 
         /// <summary>
         /// The Command Type.
         /// </summary>
-        public string CommandType { get; }
+        public string? CommandType { get; }
 
+        /// <summary>
+        /// Creates an instance of the DataAccessException Class.
+        /// </summary>
+        public DataAccessException()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Creates an instance of the DataAccessException Class.
+        /// </summary>
+        /// <param name="message">The Exception Messsage.</param>
+        public DataAccessException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Creates an instance of the DataAccessException Class.
+        /// </summary>
+        /// <param name="message">The Exception Messsage.</param>
+        /// <param name="innerException">The Inner Exception.</param>
+        public DataAccessException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new Instance of the DataAccessException Class. 
+        /// </summary>
+        /// <param name="message">The Exception Messsage.</param>
+        /// <param name="commandText">The Command Text.</param>
+        /// <param name="commandType">The Command Type.</param>
         public DataAccessException(string message, string commandText, string commandType)
             : base(message)
         {
@@ -24,6 +57,13 @@ namespace SQLDataAccess.Common.Exceptions
             this.CommandType = commandType;
         }
 
+        /// <summary>
+        /// Creates a new Instance of the DataAccessException Class. 
+        /// </summary>
+        /// <param name="message">The Exception Messsage.</param>
+        /// <param name="innerException">The Inner Exception.</param>
+        /// <param name="commandText">The Command Text.</param>
+        /// <param name="commandType">The Command Type.</param>
         public DataAccessException(
             string message,
             Exception innerException,
--- SQLDataAccess/SQLServer/Exceptions/SqlServerDataAccessException.cs
+++ SQLDataAccess/SQLServer/Exceptions/SqlServerDataAccessException.cs
@@ -1,15 +1,95 @@
-﻿namespace SQLDataAccess.SQLServer.Exceptions
+﻿// "<copyright file="SqlServerDataAccessException.cs">
+// Copyright (c) Advaith Harikrishnan. All rights reserved.
+// </copyright>"
+
+namespace SQLDataAccess.SQLServer.Exceptions
 {
     using System;
-    using System.Runtime.Serialization;
-    using System.Security.Permissions;
     using Microsoft.Data.SqlClient;
+    using SQLDataAccess.Common.Exceptions;
 
     /// <summary>
-    /// Class for Custom SQL Exception.
+    /// Exception Class to deal with SQL Server DataAccess Exceptions.
     /// </summary>
     [Serializable]
-    public class SqlServerDataAccessException : Exception
+    public class SqlServerDataAccessException : DataAccessException
     {
+        /// <summary>
+        /// The Sql Parameters as a string.
+        /// </summary>
+        public string? SqlParameters { get; }
+        
+        /// <summary>
+        /// The MS Sql Exception Instance.
+        ///  Contains more exact information about the exception.
+        /// </summary>
+        public SqlException? sqlException { get; }
+
+        /// <summary>
+        /// Creates an instance of the SqlServerDataAccessException class 
+        /// </summary>
+        public SqlServerDataAccessException()
+        {}
+
+        /// <summary>
+        /// Creates an instance of the SqlServerDataAccessException class 
+        /// </summary>
+        /// <param name="message">The Exception Message.</para>
+        public SqlServerDataAccessException(string message)
+            : base (message)
+        {}
+
+        /// <summary>
+        /// Creates an instance of the SqlServerDataAccessException class 
+        /// </summary>
+        /// <param name="message">The Exception Message.</param>
+        /// <param name="commandText">The Command Text.</param>
+        /// <param name="commandType">The Command Type.</param>
+        public SqlServerDataAccessException(string message, string commandText, string commandType)
+            : base (message, commandText, commandType)
+        {}
+
+        /// <summary>
+        /// Creates an instance of the SqlServerDataAccessException class 
+        /// </summary>
+        /// <param name="message">The Exception Message.</param>
+        /// <param name="commandText">The Command Text.</param>
+        /// <param name="commandType">The Command Type.</param>
+        /// <param name="sqlParameters">The SQL Parameters.</param>
+        /// <param name="sqlException">The MS SQL Exception.</param>
+        public SqlServerDataAccessException(string message, string commandText, string commandType, SqlParameter[]? sqlParameters, SqlException sqlException)
+            : base (message, commandText, commandType)
+        {
+            this.sqlException = sqlException;
+            this.SqlParameters = ParseSqlParameters(sqlParameters);
+        }
+
+        /// <summary>
+        /// Creates an instance of the SqlServerDataAccessException class 
+        /// </summary>
+        /// <param name="message">The Exception Message.</param>
+        /// <param name="sqlException">The MS SQL Exception.</param>
+        public SqlServerDataAccessException(string message, SqlException sqlException)
+            : base (message)
+        {
+            this.sqlException = sqlException;
+        }
+
+        private string? ParseSqlParameters(SqlParameter[] sqlParameters)
+        {
+            if (sqlParameters is null || sqlParameters.Length is 0)
+                return null;
+
+            string parameters = "Parameters:\n";
+            parameters += "Name : Value\n";
+            
+            foreach(SqlParameter parameter in sqlParameters)
+            {
+                parameters += $"{parameter.ParameterName} : {parameter.Value}\n";
+            }
+
+            return parameters;
+        }
+
     }
 }
